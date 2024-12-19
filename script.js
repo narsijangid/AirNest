838e60a000919ac631b172d883464175872b20c5
narsi-jangid <narsijangid01@gmail.com>
2024-12-19 10:49:38 +0530

    signup popup added

--- script.js
+++ script.js
@@ -3,3 +3,43 @@ function toggleMenu() {
     
     menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
 }
+
+
+document.getElementById('languageIcon').addEventListener('click', function () {
+    document.getElementById('languagePopup').style.display = 'block';
+});
+
+document.getElementById('closePopup').addEventListener('click', function () {
+    document.getElementById('languagePopup').style.display = 'none';
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const authPopup = document.getElementById('authPopup');
+    const closeAuthPopupBtn = document.getElementById('closeAuthPopup');
+    const menuLinks = document.querySelectorAll('.menu a');
+
+  
+    menuLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            if (event.target.textContent === 'Sign up' || event.target.textContent === 'Log in') {
+                event.preventDefault();
+                authPopup.style.display = 'flex';
+            }
+        });
+    });
+
+
+    closeAuthPopupBtn.addEventListener('click', () => {
+        authPopup.style.display = 'none';
+    });
+
+
+    authPopup.addEventListener('click', event => {
+        if (event.target === authPopup) {
+            authPopup.style.display = 'none';
+        }
+    });
+});
